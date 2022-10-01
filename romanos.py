0bc70435a8e78e1224ac25ac553b56e5d786fa34
whitewolf <santidotpy@gmail.com>
2022-10-01 19:56:24 -0300

    ✨ update romanos

--- romanos.py
+++ romanos.py
@@ -1,11 +1,11 @@
 
 
 
-def romanNumbers(n):
+def decimalToRoman(n):
 
     if n >= 4000:
         return False
-  
+
     m = ["", "M", "MM", "MMM"]
     c = ["", "C", "CC", "CCC", "CD", "D",
          "DC", "DCC", "DCCC", "CM"]
@@ -13,17 +13,50 @@ def romanNumbers(n):
          "LX", "LXX", "LXXX", "XC"]
     i = ["", "I", "II", "III", "IV", "V",
          "VI", "VII", "VIII", "IX"]
-  
+
     thousands = m[n // 1000]
     hundreds = c[(n % 1000) // 100]
     tens = x[(n % 100) // 10]
     ones = i[n % 10]
-  
+
     roman = (thousands + hundreds +
            tens + ones)
-  
+
     return roman
-  
+
+
+def romanToDecimal(roman):
+     roman = roman.upper()
+     roman_values = {    'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                         'C':  100, 'D': 500, 'M': 1000
+                    }
+     int_value = 0
+
+     # check invalid characters
+     characters = []
+     for char in roman:
+          characters.append(char)
+          if char not in roman_values.keys():
+               return False
+
+     if check_four_consecutive(roman):
+          return False
+
+     for i in range(len(roman)):
+          if i > 0 and roman_values[roman[i]] > roman_values[roman[i - 1]]:
+               int_value += roman_values[roman[i]] - 2 * roman_values[roman[i - 1]]
+          else:
+               int_value += roman_values[roman[i]]
+     return int(int_value)
+
+def check_four_consecutive(roman):
+     for char in roman:
+          if char*4 in roman:
+               return True
+     return False
+
+
 # if __name__ == "__main__":
+#     print(romanToDecimal('XXXX'))
 #     number = 3999
 #     print(intToRoman(number))
--- test_romanos.py
+++ test_romanos.py
@@ -3,18 +3,42 @@ import romanos
 
 class Test_FourInLine(unittest.TestCase):
     
-    def test_numbers(self):
+    def test_decimalToRoman(self):
         params = [  (1, 'I'), (4, 'IV'), (5, 'V'), (9, 'IX'), (10, 'X'),
                     (40, 'XL'), (50, 'L'), (90, 'XC'), (100, 'C'), (400, 'CD'),
                     (500, 'D'), (900, 'CM'), (1000, 'M'), (1549, 'MDXLIX'), (3999, 'MMMCMXCIX')]
         for p1, p2 in params:
             with self.subTest():
-                self.assertEqual(p2, romanos.romanNumbers(p1))
+                self.assertEqual(p2, romanos.decimalToRoman(p1))
 
     def test_high_number(self):
-        number = romanos.romanNumbers(4000)
+        number = romanos.decimalToRoman(4000)
         self.assertFalse(number)
-        
+
+    def test_romanToDecimal(self):
+        params = [  ('I', 1), ('IV', 4), ('V', 5), ('IX', 9), ('X', 10),
+                    ('XL', 40), ('L', 50), ('XC', 90), ('C', 100), ('CD', 400),
+                    ('D', 500), ('CM', 900), ('M', 1000), ('MDXLIX', 1549), ('MMMCMXCIX', 3999)]
+        for p1, p2 in params:
+            with self.subTest():
+                self.assertEqual(p2, romanos.romanToDecimal(p1))
+
+    def test_wrong_value(self):
+        number = romanos.romanToDecimal('a')
+        self.assertFalse(number)
+
+    def test_consecutive(self):
+        number = romanos.romanToDecimal('XXXX')
+        self.assertFalse(number)        
+
+    def test_check_four_consecutive(self):
+        number = romanos.check_four_consecutive('XXXDDDMMMLLLL')
+        self.assertTrue(number)
+
+    def test_check_four_consecutive_false(self):
+        number = romanos.check_four_consecutive('XXX')
+        self.assertFalse(number)     
+
 
 if __name__ == '__main__':
     unittest.main()
